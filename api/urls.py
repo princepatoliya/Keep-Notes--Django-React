5696d492a3e4d5dcd42502c82d87355e6daf0de8
Prince Patoliya <princepatoliya852@gmail.com>
2021-09-25 14:07:28 +0530

    CRUD operations complete and debug some code

--- api/urls.py
+++ api/urls.py
@@ -3,11 +3,17 @@ from django.urls import path, include
 from .views import (
     getNotes,
     getRoutes,
-    getNote
+    getNote,
+    createNote,
+    updateNote,
+    deleteNote,
 )
 
 urlpatterns = [
     path('', getRoutes, name="getRoutes"),
     path('notes/', getNotes, name="getNotes"),
+    path('notes/create/', createNote, name="create-note"),
+    path('notes/<int:pk>/update/', updateNote, name="update-note"),
+    path('notes/<int:pk>/delete/', deleteNote, name="delete-note"),
     path('notes/<int:pk>/', getNote, name="getNote"),
 ]
--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
 from django.contrib.admin.options import IS_POPUP_VAR
+from django.db.models import manager
 from django.shortcuts import render
 
 from rest_framework.response import Response
@@ -60,3 +61,31 @@ def getNote(request, pk):
     serializer = NoteSerializer(notes, many=False)
     return Response(serializer.data)
 
+@api_view(["POST"])
+def createNote(request):
+    data = request.data
+    note = Note.objects.create(
+        body=data['body']
+    )
+    serializer = NoteSerializer(note, many = False)
+    return Response(serializer.data)
+
+
+@api_view(['PUT'])
+def updateNote(request, pk):
+    data = request.data
+    note = Note.objects.get(id=pk)
+    serializer = NoteSerializer(instance = note, data = data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+
+@api_view(["DELETE"])
+def deleteNote(request, pk):
+    note = Note.objects.get(id=pk)
+    note.delete()
+
+    return Response("note deleted.")
